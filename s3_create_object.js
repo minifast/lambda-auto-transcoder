0c950bb0433ada9e9393a84fa81b352028361f6b
Doc Ritezel <pair+doc@ministryofvelocity.com>
2015-07-20 00:48:58 -0700

    Add elastic transcoder forwarding

--- s3_create_object.js
+++ s3_create_object.js
@@ -1,11 +1,64 @@
 var aws = require("aws-sdk");
 var s3 = new aws.S3({apiVersion: "2006-03-01"});
+var et = new aws.ElasticTranscoder({apiVersion: '2012-09-25', region: 'us-west-2'});
 
-var UploadedObject = function(event, context) {
+var etPipelineId = "1437374070396-179a4g";
+var etPresetId = "1351620000001-100070";
+
+var responseHandlerCurrier = function currier() {
+  var self = this;
+  return function boundResponseHandler(error, data) {
+    if (error) {
+      self.handleError(error);
+    } else {
+      self.handleSuccess(data);
+    }
+  };
+};
+
+var UntranscodedObject = function(bucket, key, context) {
+  this.bucket = bucket;
+  this.key = key;
+  this.context = context;
+};
+
+UntranscodedObject.prototype.toParameters = function untranscodedObjectParameters() {
+  return {
+    PipelineId: etPipelineId,
+    OutputKeyPrefix: "encoded/",
+    Input: {
+      Key: this.key,
+      FrameRate: "auto",
+      Resolution: "auto",
+      AspectRatio: "auto",
+      Interlaced: "auto",
+      Container: "auto"
+    },
+    Outputs: [
+      {
+        Key: this.key + ".mp4",
+        PresetId: etPresetId
+      }
+    ]
+  };
+};
+
+UntranscodedObject.prototype.handleError = function untranscodedErrorHandler() {
+  this.context.fail("Failed to send new video " + this.key + " to ET");
+};
+
+UntranscodedObject.prototype.handleSuccess = function untranscodedSuccessHandler() {
+  this.context.succeed("Encoded " + this.key + " successfully!");
+};
+
+UntranscodedObject.prototype.responseHandler = responseHandlerCurrier;
+
+var UploadedObject = function(event, context, et) {
   this.bucket = event.Records[0].s3.bucket.name;
   this.key = event.Records[0].s3.object.key;
   this.context = context;
-}
+  this.et = et;
+};
 
 UploadedObject.prototype.toParameters = function uploadObjectParameters() {
   return {
@@ -14,29 +67,21 @@ UploadedObject.prototype.toParameters = function uploadObjectParameters() {
   };
 };
 
-UploadedObject.prototype.handleError = function uploadErrorHandler(_) {
-  var message = "Error getting " + this.key + " from " + this.bucket;
-  this.context.fail(message);
+UploadedObject.prototype.handleError = function uploadErrorHandler() {
+  this.context.fail("Error getting " + this.key + " from " + this.bucket);
 };
 
-UploadedObject.prototype.handleSuccess = function uploadSuccessHandler(data) {
-  this.context.succeed(data.ContentType);
+UploadedObject.prototype.handleSuccess = function uploadSuccessHandler() {
+  var untranscodedObject = new UntranscodedObject(this.bucket, this.key, this.context);
+  this.et.createJob(untranscodedObject.toParameters(), responseHandlerCurrier(untranscodedObject));
 };
 
-UploadedObject.prototype.responseHandler = function uploadResponseHandler() {
-  var self = this;
-  return (function boundResponseHandler(error, data) {
-    if (error) {
-      self.handleError(error);
-    } else {
-      self.handleSuccess(data);
-    }
-  });
-};
+UploadedObject.prototype.responseHandler = responseHandlerCurrier;
 
 exports.UploadedObject = UploadedObject;
+exports.UntranscodedObject = UntranscodedObject;
 
 exports.handler = function(event, context) {
-  var uploadedObject = new UploadedObject(event, context);
-  s3.getObject(uploadedObject.toParameters(), uploadedObject.responseHandler());
+  var uploadedObject = new UploadedObject(event, context, et);
+  s3.headObject(uploadedObject.toParameters(), uploadedObject.responseHandler());
 };
--- spec/s3_create_object_spec.js
+++ spec/s3_create_object_spec.js
@@ -1,8 +1,70 @@
 var s3CreateObject = require('../s3_create_object');
 
 describe("S3CreateObject", function() {
+  beforeEach(function() {
+    this.succeedSpy = jasmine.createSpy();
+    this.failSpy = jasmine.createSpy();
+    this.createJobSpy = jasmine.createSpy();
+  });
+
   it("loads the handler", function() {
     expect(typeof(s3CreateObject.handler)).toEqual('function');
+  });
+
+  describe("UntranscodedObject", function() {
+    beforeEach(function() {
+      var bucket = "fish";
+      var key = "rusty";
+      var context = {succeed: this.succeedSpy, fail: this.failSpy};
+      this.untranscodedObject = new s3CreateObject.UntranscodedObject(bucket, key, context);
+    });
+
+    describe("#toParameters", function() {
+      it("presents some configuration for an elastic transcoder job", function() {
+        expect(this.untranscodedObject.toParameters()).toEqual(
+          jasmine.objectContaining({
+            OutputKeyPrefix: 'encoded/',
+            Input: jasmine.objectContaining({Key: 'rusty'}),
+            Outputs: [jasmine.objectContaining({Key: 'rusty.mp4'})]
+          })
+        );
+      });
+    });
+
+    describe("#handleSuccess", function() {
+      it("calls the succeed handler with the content type", function() {
+        this.untranscodedObject.handleSuccess(this.data);
+        expect(this.succeedSpy).toHaveBeenCalledWith(
+          "Encoded rusty successfully!"
+        );
+      });
+    });
+
+    describe("#handleError", function() {
+      it("calls the succeed handler with the content type", function() {
+        this.untranscodedObject.handleError("meh");
+        expect(this.failSpy).toHaveBeenCalledWith(
+          "Failed to send new video rusty to ET"
+        );
+      });
+    });
+
+    describe("#responseHandler", function() {
+      beforeEach(function (){
+        spyOn(this.untranscodedObject, "handleSuccess");
+        spyOn(this.untranscodedObject, "handleError");
+      });
+
+      it("returns a curried response handler for the success case", function() {
+        this.untranscodedObject.responseHandler()(null, this.data);
+        expect(this.untranscodedObject.handleSuccess).toHaveBeenCalled();
+      });
+
+      it("returns a curried response handler for the failure case", function() {
+        this.untranscodedObject.responseHandler()("wat", null);
+        expect(this.untranscodedObject.handleError).toHaveBeenCalled();
+      });
+    });
   });
 
   describe("UploadedObject", function() {
@@ -10,10 +72,10 @@ describe("S3CreateObject", function() {
       var bucket = {name: "lunchpail"};
       var object = {key: "mustang"};
       var event = {Records: [{s3: {bucket: bucket, object: object}}]};
-      this.succeedSpy = jasmine.createSpy();
-      this.failSpy = jasmine.createSpy();
+      var et = {createJob: this.createJobSpy};
       var context = {succeed: this.succeedSpy, fail: this.failSpy};
-      this.uploadedObject = new s3CreateObject.UploadedObject(event, context);
+      this.uploadedObject = new s3CreateObject.UploadedObject(event, context, et);
+      this.data = {};
     });
 
     describe("#toParameters", function() {
@@ -26,13 +88,11 @@ describe("S3CreateObject", function() {
     });
 
     describe("#handleSuccess", function() {
-      beforeEach(function (){
-        this.data = {ContentType: "taco/meat"};
-      });
-
       it("calls the succeed handler with the content type", function() {
         this.uploadedObject.handleSuccess(this.data);
-        expect(this.succeedSpy).toHaveBeenCalledWith("taco/meat");
+        expect(this.createJobSpy).toHaveBeenCalledWith(
+          jasmine.any(Object), jasmine.any(Function)
+        );
       });
     });
 
@@ -54,7 +114,7 @@ describe("S3CreateObject", function() {
       });
 
       it("returns a curried response handler for the success case", function() {
-        this.uploadedObject.responseHandler()(null, {});
+        this.uploadedObject.responseHandler()(null, this.data);
         expect(this.uploadedObject.handleSuccess).toHaveBeenCalled();
       });
 
